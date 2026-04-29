70e8b9736ce7482a2ddd27b1bc5d091b01cb80e7
Brian <brian9206@hotmail.com>
2026-04-29 11:16:47 +0800

    chore: prettier setup

--- invoke-admin/lib/monaco-editor.ts
+++ invoke-admin/lib/monaco-editor.ts
@@ -2,14 +2,14 @@ import Editor, { loader } from '@monaco-editor/react'
 
 loader.config({
   paths: {
-    vs: '/monaco/vs',
-  },
+    vs: '/monaco/vs'
+  }
 })
 
 // Disable built-in TS/JS hover providers BEFORE any editor is created.
 // We replace them with a custom hover in onMount that overrides `any`-typed
 // req/res/next params with InvokeRequest/InvokeResponse types.
-loader.init().then((monaco) => {
+loader.init().then(monaco => {
   const noHovers = {
     completionItems: true,
     hovers: false,
@@ -23,7 +23,7 @@ loader.init().then((monaco) => {
     signatureHelp: true,
     onTypeFormattingEdits: true,
     codeActions: true,
-    inlayHints: true,
+    inlayHints: true
   }
   monaco.languages.typescript.typescriptDefaults.setModeConfiguration(noHovers)
   monaco.languages.typescript.javascriptDefaults.setModeConfiguration(noHovers)
